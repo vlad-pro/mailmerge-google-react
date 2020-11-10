21d4aca787f99a4f5bff7c77fe6364f1eeb9bcea
vproskurin <vladimir.proskurin@gmail.com>
2020-11-09 23:19:48 -0800

    decoupling and moving fucntionality away

--- src/App.js
+++ src/App.js
@@ -1,165 +1,78 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-import axios from "axios";
+import { 
+  generateAuthCodeUrl, 
+  getAccessToken, 
+  copyGoogleDoc, 
+  batchUpdate
+} from "./services";
 
-// authorization secrets
-const client_id =process.env.REACT_APP_CLIENT_ID;
-const client_secret = process.env.REACT_APP_CLIENT_SECRET;
 
-// authorization constants
-const scope = "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/documents";
-const redirect_uri = "http://localhost:3000";
-const response_type = "code";
+export function App2() {
+  const [to_name, setTo_name] = useState("");
+  const [to_title, setTo_title] = useState("");
+  const [to_company, setTo_company] = useState("");
+  const [to_address, setTo_address] = useState("");
 
-// application constants
-const DOCS_FILE_ID = "10Ob_wgq4QEp8eNZwb_ei2sSTPrQgEHcnCsa4S4e4ndE";
-
-// data/body for mailMerge call
-const body = {
-  "requests": [
-    {
-      replaceAllText: {
-        containsText: {
-          text: '{{MY_NAME}}',
-          matchCase: true,
-        },
-        replaceText: "Ms. Lara Brown",
-      },
-    },
-    {
-      replaceAllText: {
-        containsText: {
-          text: '{{MY_ADDRESS}}',
-          matchCase: true,
-        },
-        replaceText: "360 South Maple Drive,\nBeverly Hills,\nCalifornia 90212",
-      },
-    },
-    {
-      replaceAllText: {
-        containsText: {
-          text: '{{MY_PHONE}}',
-          matchCase: true,
-        },
-        replaceText: "+1(234) 567-8910",
-      },
-    },
-    {
-      replaceAllText: {
-        containsText: {
-          text: '{{MY_EMAIL}}',
-          matchCase: true,
-        },
-        replaceText: "lara@borrowlabs.com",
-      },
-    },
-    {
-      replaceAllText: {
-        containsText: {
-          text: '{{DATE}}',
-          matchCase: true,
-        },
-        replaceText: "2020-11-11",
-      },
-    },
-    {
-      replaceAllText: {
-        containsText: {
-          text: '{{TO_NAME}}',
-          matchCase: true,
-        },
-        replaceText: "Ms. John Dow",
-      },
-    },
-    {
-      replaceAllText: {
-        containsText: {
-          text: '{{TO_TITLE}}',
-          matchCase: true,
-        },
-        replaceText: "Appraiser",
-      },
-    },
-    {
-      replaceAllText: {
-        containsText: {
-          text: '{{TO_COMPANY}}',
-          matchCase: true,
-        },
-        replaceText: "Best South Appraisal",
-      },
-    },
-    {
-      replaceAllText: {
-        containsText: {
-          text: '{{TO_ADDRESS}}',
-          matchCase: true,
-        },
-        replaceText: "939 Plam Drive,\nIrvine,\nCalifornia 80404",
-      },
-    },
-    {
-      replaceAllText: {
-        containsText: {
-          text: '{{BODY}}',
-          matchCase: true,
-        },
-        replaceText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      },
-    },
-  ]
-}
-
-const generateAuthCodeUrl = function () {
-  var authURL =
-    "https://accounts.google.com/o/oauth2/v2/auth?" +
-    "client_id=" + client_id +
-    "&scope=" + scope +
-    "&redirect_uri=" + redirect_uri +
-    "&response_type=" + response_type;
-
-  // use this url to redirect user to consent page
-  return authURL;
-};
-
-// Get AuthToken from google
-const getAccessToken = async (x) => {
-      var postDataUrl = 'https://www.googleapis.com/oauth2/v4/token?' +
-          'code=' + x +  //auth code received from the previous call
-          '&client_id=' + client_id +
-          '&client_secret=' + client_secret +
-          '&redirect_uri=' + redirect_uri +
-          "&grant_type=" + 
-          "authorization_code";
-
-      return await axios.post(postDataUrl)
-      .then((response) =>  response.data.access_token)
-
-  };
-
-const copyGoogleDoc = async (token) => {
-  var copyDocUrl = "https://www.googleapis.com/drive/v3/files/" + DOCS_FILE_ID + "/copy";
-
-  return await axios.post(copyDocUrl,null,{
-    headers:{
-      "Content-type": "application/json",
-      "Authorization": "Bearer " + token
+  useEffect(()=> {
+    function handleSubmit(event) {
+      alert("The following was submitted: " + this.state.to_title);
+      event.preventDefault();
     }
   })
-  .then(response =>  response.data.id);
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="to_name"
+              onChange={setTo_name}
+              // placeholder="Name of the recipient"
+            />
+          </label>
+
+          <label>
+            Title:
+            <input
+              type="text"
+              name="to_title"
+              onChange={setTo_title}
+              // placeholder="Title of the recipient"
+            />
+          </label>
+
+          <label>
+            Title:
+            <input
+              type="text"
+              name="to_company"
+              onChange={setTo_company}
+              // placeholder="Company of the recipient"
+            />
+          </label>
+
+          <label>
+            Address:
+            <input
+              type="text"
+              name="to_address"
+              onChange={setTo_address}
+              // placeholder="Address of the recipient"
+            />
+          </label>
+
+          <input type="submit" value="Submit" />
+        </form>
+
+        <button onClick={this.mailMerge}>Create Document From Template</button>
+        <button onClick={this.urlDisplay}>display url</button>
+      </div>
+    );
 }
 
-const batchUpdate = async (doc_id, token) => {
-  var batchDocUrl = "https://docs.googleapis.com/v1/documents/" + doc_id + ":batchUpdate"
-  
-  return await axios.post(batchDocUrl, body,{
-    headers:{
-      "Content-type": "application/json",
-      "Authorization": "Bearer " + token
-    }
-  })
-  .then(response =>  response.status);
-}
 
 export class App extends React.Component {
   constructor(props) {
@@ -280,7 +193,3 @@ export class App extends React.Component {
     );
   }
 }
-
-function App2() {}
-
-
--- src/services.js
+++ src/services.js
@@ -1,29 +1,165 @@
-exports.generateAuthCodeUrl = function (client_id, scope, redirect_uri, response_type) {
+import { axios } from "axios";
 
-    var authURL = "https://accounts.google.com/o/oauth2/v2/auth?" +
-        "client_id=" + client_id +
-        "&scope=" + scope +
-        "&redirect_uri=" + redirect_uri +
-        "&response_type=" + response_type;
+// authorization secrets
+const client_id =process.env.REACT_APP_CLIENT_ID;
+const client_secret = process.env.REACT_APP_CLIENT_SECRET;
 
-    //redirect to consent page
-    return authURL;  
+// authorization constants
+const scope = "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/documents";
+const redirect_uri = "http://localhost:3000";
+const response_type = "code";
+
+// application constants
+const DOCS_FILE_ID = "10Ob_wgq4QEp8eNZwb_ei2sSTPrQgEHcnCsa4S4e4ndE";
+
+
+export const generateAuthCodeUrl = function () {
+  var authURL =
+    "https://accounts.google.com/o/oauth2/v2/auth?" +
+    "client_id=" + client_id +
+    "&scope=" + scope +
+    "&redirect_uri=" + redirect_uri +
+    "&response_type=" + response_type + 
+    "&access_type" + 
+    "offline";
+
+  // use this url to redirect user to consent page
+  return authURL;
 };
 
-// exports.getAccessToken = function (x) {
-//     var postDataUrl = 'https://www.googleapis.com/oauth2/v4/token?' +
-//         'code=' + x +  //auth code received from the previous call
-//         '&client_id=' + client_id +
-//         '&client_secret=' + client_secret +
-//         '&redirect_uri=' + redirect_uri +
-//         '&grant_type=' + "authorization_code"
+// Get AuthToken from google
+export const getAccessToken = async (x) => {
+      var postDataUrl = 'https://www.googleapis.com/oauth2/v4/token?' +
+          'code=' + x +  //auth code received from the previous call
+          '&client_id=' + client_id +
+          '&client_secret=' + client_secret +
+          '&redirect_uri=' + redirect_uri +
+          "&grant_type=" + 
+          "authorization_code";
 
-//     var options = {
-//         uri: postDataUrl,
-//         method: 'POST'
-//     };
+      return await axios.post(postDataUrl)
+      .then((response) =>  response.data.access_token)
 
-//     request(options, function (err, res, body) {
-//         return body; //returns an object with an access token!!!
-//     });
-// };
+  };
+
+export const copyGoogleDoc = async (token) => {
+  var copyDocUrl = "https://www.googleapis.com/drive/v3/files/" + DOCS_FILE_ID + "/copy";
+
+  return await axios.post(copyDocUrl,null,{
+    headers:{
+      "Content-type": "application/json",
+      "Authorization": "Bearer " + token
+    }
+  })
+  .then(response =>  response.data.id);
+}
+
+export const batchUpdate = async (doc_id, token) => {
+  var batchDocUrl = "https://docs.googleapis.com/v1/documents/" + doc_id + ":batchUpdate"
+  
+  return await axios.post(batchDocUrl, body,{
+    headers:{
+      "Content-type": "application/json",
+      "Authorization": "Bearer " + token
+    }
+  })
+  .then(response =>  response.status);
+}
+
+
+// data/body for mailMerge call
+const body = {
+  "requests": [
+    {
+      replaceAllText: {
+        containsText: {
+          text: '{{MY_NAME}}',
+          matchCase: true,
+        },
+        replaceText: "Ms. Lara Brown",
+      },
+    },
+    {
+      replaceAllText: {
+        containsText: {
+          text: '{{MY_ADDRESS}}',
+          matchCase: true,
+        },
+        replaceText: "360 South Maple Drive,\nBeverly Hills,\nCalifornia 90212",
+      },
+    },
+    {
+      replaceAllText: {
+        containsText: {
+          text: '{{MY_PHONE}}',
+          matchCase: true,
+        },
+        replaceText: "+1(234) 567-8910",
+      },
+    },
+    {
+      replaceAllText: {
+        containsText: {
+          text: '{{MY_EMAIL}}',
+          matchCase: true,
+        },
+        replaceText: "lara@borrowlabs.com",
+      },
+    },
+    {
+      replaceAllText: {
+        containsText: {
+          text: '{{DATE}}',
+          matchCase: true,
+        },
+        replaceText: "2020-11-11",
+      },
+    },
+    {
+      replaceAllText: {
+        containsText: {
+          text: '{{TO_NAME}}',
+          matchCase: true,
+        },
+        replaceText: "Ms. John Dow",
+      },
+    },
+    {
+      replaceAllText: {
+        containsText: {
+          text: '{{TO_TITLE}}',
+          matchCase: true,
+        },
+        replaceText: "Appraiser",
+      },
+    },
+    {
+      replaceAllText: {
+        containsText: {
+          text: '{{TO_COMPANY}}',
+          matchCase: true,
+        },
+        replaceText: "Best South Appraisal",
+      },
+    },
+    {
+      replaceAllText: {
+        containsText: {
+          text: '{{TO_ADDRESS}}',
+          matchCase: true,
+        },
+        replaceText: "939 Plam Drive,\nIrvine,\nCalifornia 80404",
+      },
+    },
+    {
+      replaceAllText: {
+        containsText: {
+          text: '{{BODY}}',
+          matchCase: true,
+        },
+        replaceText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      },
+    },
+  ]
+}
+
